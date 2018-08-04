9ed1dd1f805408b7cd1d7a52c52f3ef311253a49
Netanel Gilad <netanel@botique.ai>
2018-08-04 09:44:00 +0300

    feat(Variable): defer getting value

--- packages/core/src/Variable.tsx
+++ packages/core/src/Variable.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 import { Renderable } from "./Renderable";
 
-export type VariableProps<T> = {
+export type VariableProps<T extends object> = {
   initialValue: T;
   children: Renderable<{
-    value: T;
+    getValue: () => T;
     setValue(partial: Partial<T>);
   }>;
 };
 
-export class Variable<T> extends React.Component<VariableProps<T>, {}> {
+export class Variable<T extends object> extends React.Component<VariableProps<T>, {}> {
   value: T;
 
   constructor(props: VariableProps<T>) {
@@ -20,7 +20,7 @@ export class Variable<T> extends React.Component<VariableProps<T>, {}> {
 
   render() {
     return this.props.children({
-      value: this.value,
+      getValue: () => this.value,
       setValue: partial => {
         this.value = { ...(this.value as any), ...(partial as any) };
       }
